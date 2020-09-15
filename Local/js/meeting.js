fdfd315fc8c20258b29696c7c004cbe1cbc8e3a2
Christian Nadeau <cnadeau@beehvir.com>
2020-09-14 22:19:39 -0400

    Forum support: https://devforum.zoom.us/t/i-need-some-help-with-customizing-language-when-using-web-sdk/27907/4

--- Local/js/meeting.js
+++ Local/js/meeting.js
@@ -2,6 +2,9 @@ import { ZoomMtg } from "@zoomus/websdk";
 const testTool = window.testTool;
 // get meeting args from url
 const tmpArgs = testTool.parseQuery();
+
+var meetingLanguage = tmpArgs.lang
+
 const meetingConfig = {
   apiKey: tmpArgs.apiKey,
   meetingNumber: tmpArgs.mn,
@@ -32,7 +35,7 @@ const meetingConfig = {
       return tmpArgs.email;
     }
   })(),
-  lang: tmpArgs.lang,
+  lang: meetingLanguage,
   signature: tmpArgs.signature || "",
   china: tmpArgs.china === "1",
 };
@@ -42,6 +45,8 @@ console.log(JSON.stringify(ZoomMtg.checkSystemRequirements()));
 // it's option if you want to change the WebSDK dependency link resources. setZoomJSLib must be run at first
 ZoomMtg.preLoadWasm();
 ZoomMtg.prepareJssdk();
+
+
 function beginJoin(signature) {
   ZoomMtg.init({
     leaveUrl: meetingConfig.leaveUrl,
@@ -49,6 +54,23 @@ function beginJoin(signature) {
     success: function () {
       console.log(meetingConfig);
       console.log("signature", signature);
+      
+      var translations = $.i18n.getAll("fr-FR");
+
+      var overridenTranslations = Object.assign({}, translations, {
+        'apac.wc_joining_meeting': 'Connexion en cours...',
+        'apac.wc_join_audio_by_pc': "Connecter l'audio ",
+        'apac.wc_video.start_video': "Activer",
+        'apac.wc_video.stop_video': "Désactiver",
+        'apac.toolbar_join_audio': "Rejoindre",
+        'apac.toolbar_mute': "Désactiver",
+        'apac.toolbar_muteall': "Désactiver tous",
+        'apac.toolbar_unmute': "Activer",
+        'toolbar_unmuteall': "Activer tous",
+      });
+      $.i18n.load(overridenTranslations, meetingLanguage);
+
+
       $.i18n.reload(meetingConfig.lang);
       ZoomMtg.join({
         meetingNumber: meetingConfig.meetingNumber,
